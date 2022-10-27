8b954726a40337d86c0a1db6a121c2ddebfed614
Juste Klapatauskaite <klapatauskaite.juste@accenture.com>
2022-10-27 09:26:34 +0300

    correction

--- src/main/java/management/system/library/business/mappers/AuthorMap.java
+++ src/main/java/management/system/library/business/mappers/AuthorMap.java
@@ -3,7 +3,9 @@ package management.system.library.business.mappers;
 
 import management.system.library.model.Author;
 import management.system.library.business.repository.model.AuthorDAO;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface AuthorMap {
--- src/main/java/management/system/library/business/mappers/BookMap.java
+++ src/main/java/management/system/library/business/mappers/BookMap.java
@@ -2,7 +2,9 @@ package management.system.library.business.mappers;
 
 import management.system.library.model.Book;
 import management.system.library.business.repository.model.BookDAO;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface BookMap {
